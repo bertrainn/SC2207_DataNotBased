46c9c60cd1b36ddf7f936662ef01e3fde804d04f
bertrainn <pokeflux@gmail.com>
2023-03-26 22:55:57 +0800

    updated the insert statements

---
+++ Insert/insertComplaints.sql
@@ -0,0 +1,10 @@
+INSERT INTO COMPLAINTS (COMPLAINTS_ID, TEXT, FILLED_DATE_TIME, HANDLED_DATE_TIME, CID, EID) VALUES 
+(1, 'The product was damaged when it arrived', '2022-08-02 13:00:00', '2022-08-05 09:00:00', 2, 2),
+(2, 'I received the wrong product', '2022-09-10 16:30:00', '2022-09-15 11:00:00', 3, 1),
+(3, 'The delivery was delayed', '2022-10-20 10:00:00', '2022-10-27 10:00:00', 4, 2),
+(4, 'The item is defective', '2022-11-30 14:45:00', '2022-12-07 14:45:00', 1, 3),
+(5, 'Limited selection', '2022-12-15 08:30:00', '2022-12-17 10:00:00', 1, 1),
+(6, 'Disorganized shelves', '2023-01-01 12:00:00', '2023-01-06 18:00:00', 1, 1),
+(7, 'An Employee called Bryan Tay beat me up', '2023-02-14 18:45:00', '2023-02-15 14:45:00', 1, 1), 
+(8, 'An Employee called Tan Wei Zhong stole my money', '2023-03-22 11:15:00', '2023-03-23 12:15:00', 1, 1);
+
