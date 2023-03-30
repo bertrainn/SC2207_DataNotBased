2e266f453d9d51d21da5c79f1238c10d271348be
Aloysius Tan <97999822+AloysiusTan@users.noreply.github.com>
2023-03-30 09:03:37 +0800

    Update query_9.sql

--- Queries/query_9.sql
+++ Queries/query_9.sql
@@ -1,17 +1,20 @@
-WITH purchase_counts AS (
-    SELECT s.PUBID, MONTH(io.DATE_TIME) AS month, COUNT(*) AS num_purchases
-    FROM ITEMS_IN_ORDERS io
-    JOIN STOCKS_IN_BOOKSTORE s ON io.STOCKID = s.STOCKID
-    GROUP BY s.PUBID, MONTH(io.DATE_TIME)
-), 
-increasing_pubs AS (
+SELECT p.PUBID, p.PUBLISHER, p.YEAR
+FROM (
     SELECT pc1.PUBID
-    FROM purchase_counts pc1
-    JOIN purchase_counts pc2 ON pc1.PUBID = pc2.PUBID
+    FROM (
+        SELECT s.PUBID, MONTH(io.DATE_TIME) AS month, COUNT(*) AS num_purchases
+        FROM ITEMS_IN_ORDERS io
+        JOIN STOCKS_IN_BOOKSTORE s ON io.STOCKID = s.STOCKID
+        GROUP BY s.PUBID, MONTH(io.DATE_TIME)
+    ) pc1
+    JOIN (
+        SELECT s.PUBID, MONTH(io.DATE_TIME) AS month, COUNT(*) AS num_purchases
+        FROM ITEMS_IN_ORDERS io
+        JOIN STOCKS_IN_BOOKSTORE s ON io.STOCKID = s.STOCKID
+        GROUP BY s.PUBID, MONTH(io.DATE_TIME)
+    ) pc2 ON pc1.PUBID = pc2.PUBID
     WHERE pc1.month = pc2.month - 1 AND pc1.num_purchases <= pc2.num_purchases
     GROUP BY pc1.PUBID
     HAVING COUNT(*) >= 2
-)
-SELECT p.PUBID, p.PUBLISHER, p.YEAR
-FROM PUBLICATION p
-JOIN increasing_pubs ip ON p.PUBID = ip.PUBID
+) ip
+JOIN PUBLICATION p ON ip.PUBID = p.PUBID;
